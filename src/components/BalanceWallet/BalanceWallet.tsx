05302821e272521318bcad38135a793afaa50d9f
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-09 00:34:17 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -1,43 +1,31 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
+
 import { useTonWallet } from "@tonconnect/ui-react";
-import { TonClient, WalletContractV4 } from "@ton/ton";
-import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { TonClient } from "@ton/ton";
 
 export default function BalanceWallet() {
   const [walletBalance, setWalletBalance] = useState<any | null>(null);
-  const wallet = useTonWallet();
-
-  (async function () {
-    const endpoint = await getHttpEndpoint();
-    const client = new TonClient({ endpoint });
+  const wallet = useTonWallet(); // Получаем объект кошелька
+  useEffect(() => {
     if (wallet) {
-      // Convert workchain to a number
-      const workchain = parseInt(wallet.account.chain, 10); // Assuming chain is a string
+      const client = new TonClient({ endpoint: "https://mainnet.ton.dev" }); // Создаём TON Client для mainnet (замените на нужный endpoint)
 
-      // Create publicKey Buffer if it's defined
-      let publicKeyBuffer;
-      if (wallet.account.publicKey !== undefined) {
-        publicKeyBuffer = Buffer.from(wallet.account.publicKey, "hex"); // Assuming publicKey is a hex string
-      }
+      const address = wallet.account.address;
 
-      let wallet1 = WalletContractV4.create({
-        workchain: workchain,
-        publicKey: publicKeyBuffer ?? Buffer.alloc(32), // Default value if undefined
-      });
-      // Handle the case where publicKeyBuffer is undefined
-      if (publicKeyBuffer === undefined) {
-        console.error("Wallet public key is not available.");
-        return; // Or handle the situation differently
-      }
-
-      let walletContract = client.open(wallet1);
-      let balance: bigint = await walletContract.getBalance();
-      setWalletBalance(balance);
+      client
+        // @ts-ignore
+        .getBalance(address)
+        .then((balance) => {
+          console.log("Баланс кошелька:", balance.toString());
+          setWalletBalance(balance);
+        })
+        .catch((error) => {
+          console.error("Ошибка при получении баланса:", error);
+        });
     }
-  })();
-
+  }, [wallet, walletBalance]);
   return (
     <Section header="Balance">
       {JSON.stringify(walletBalance)}
