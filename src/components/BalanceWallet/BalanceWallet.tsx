701f10afb0d99ada211b11aa967239480978118a
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-08 21:20:05 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -1,24 +1,44 @@
 "use client";
-import { useState, useEffect } from "react";
-import { useTonWallet } from "@tonconnect/ui-react";
+import { useState, useEffect, useCallback } from "react";
+import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
+import { TonClient } from "@tonclient/core";
 
 export default function BalanceWallet() {
   const wallet = useTonWallet();
-  const [tokenBalance, setTokenBalance] = useState(null);
+  const client = new TonClient();
+  const [walletBalance, setWalletBalance] = useState();
+  const getBalance = useCallback(async () => {
+    try {
+      const result = await client.request("get_account_state", {
+        address: wallet?.account.address,
+      });
+
+      if (result.account_state) {
+        const balance = result.account_state.balance;
+        console.log("Баланс кошелька:", balance);
+        setWalletBalance(balance);
+      } else {
+        console.log("Ошибка: аккаунт не найден");
+      }
+    } catch (error) {
+      console.error("Ошибка получения баланса:", error);
+    }
+  }, [client, wallet]);
 
   useEffect(() => {
+    getBalance();
     console.log(wallet);
-  }, [wallet]);
+  }, [wallet, getBalance]);
 
   return (
     <Section header="Balance">
-      {wallet && JSON.stringify(wallet)}
-      {tokenBalance !== null && (
+      {wallet && JSON.stringify(walletBalance)}
+      {walletBalance !== null && (
         <Cell
           after={
             <Info subtitle="balance" type="text">
-              {tokenBalance} YOUR_TOKEN_SYMBOL
+              {walletBalance} YOUR_TOKEN_SYMBOL
             </Info>
           }
           before={<Avatar size={48} />}
