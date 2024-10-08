5f03fc58084edd614f4cdc6d575b1efdb798f9e7
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-08 23:43:29 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -1,53 +1,32 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useTonWallet } from "@tonconnect/ui-react";
+import { useState, useEffect } from "react";
 import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
-import { TonClient } from "@tonclient/core";
-
+import { useTonWallet } from "@tonconnect/ui-react";
 export default function BalanceWallet() {
-  const wallet = useTonWallet();
   const [walletBalance, setWalletBalance] = useState<any | null>(null);
-  const userAddress = wallet?.account.address;
-  const DappServer = "net.ton.dev";
-  const client = new TonClient({ network: { endpoints: [DappServer] } });
-
-  let getBalance = async function (address: string) {
-    try {
-      console.log("address data", address);
-      const batchQueryResult = (
-        await client.net.batch_query({
-          operations: [
-            {
-              type: "QueryCollection",
-              collection: "accounts",
-              filter: {
-                id: {
-                  eq: address,
-                },
-              },
-              result: "balance",
-            },
-          ],
-        })
-      ).results;
-      if (!batchQueryResult[0][0]) {
-        throw Error;
-      } else {
-        let yourNumber = parseInt(batchQueryResult[0][0].balance, 16);
-        console.log("Balance of wallet 1 is " + yourNumber + " grams");
-        setWalletBalance(batchQueryResult);
-        return { address: address, balance: yourNumber };
-      }
-    } catch (error: any) {
-      console.error(error);
-      throw [error.name, error.message];
-    }
-  };
-
+  const wallet = useTonWallet();
   useEffect(() => {
-    getBalance(userAddress || "");
-  }, [userAddress]);
-
+    fetch("https://ton.org/services/jsonrpc", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: 1,
+        method: "get_balance",
+        params: [wallet?.account.address],
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.result);
+        setWalletBalance(data.result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [wallet]);
   return (
     <Section header="Balance">
       {JSON.parse(walletBalance)}
