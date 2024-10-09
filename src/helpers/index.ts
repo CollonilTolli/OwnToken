c55469f58ca830d247deeb902ddef6713301edcd
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-10 01:47:35 +0300

    check get balance

---
+++ src/helpers/index.ts
@@ -0,0 +1,20 @@
+export async function fetchJettonData(url: string) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.error("Ошибка:", error);
+    return null
+  }
+}
