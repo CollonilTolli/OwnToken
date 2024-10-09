7432b85792b96ed9ab2db7d6a0772e242e2c412f
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-09 03:37:17 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -5,32 +5,51 @@ import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
 import { useTonWallet } from "@tonconnect/ui-react";
 
 export default function BalanceWallet() {
-  const [walletBalance, setWalletBalance] = useState<number | null>(null);
+  const [walletTokens, setWalletTokens] = useState<
+    { name: string; balance: number }[]
+  >([]);
 
   const wallet = useTonWallet();
   const address = wallet?.account?.address;
-  useEffect(() => {
-    const url = `https://toncenter.com/api/v2/getAddressInformation?address=${address}`;
-    if (address) {
-      fetch(url)
-        .then(async (response: any) => {
-          const res = await response.json();
-          console.log(res);
-          setWalletBalance(parseFloat(res.result.balance) / 1e9);
-        })
-        .catch((error) => console.error(error));
-    }
-  }, [address, walletBalance]);
 
-  return walletBalance !== null ? (
+  useEffect(() => {
+    const fetchTokenBalances = async () => {
+      if (address) {
+        const url = `https://toncenter.com/api/v2/getAddressInformation?address=${address}`;
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+
+          // Extract tokens from the API response
+          const tokens = Object.entries(data.result.balance)
+            .filter(([key, value]) => key !== "balance")
+            .map(([key, value]) => ({
+              name: key, //@ts-ignore
+              balance: parseFloat(value) / 1e9, // Convert balance to human-readable format
+            }));
+
+          setWalletTokens(tokens);
+        } catch (error) {
+          console.error("Error fetching token balances:", error);
+        }
+      }
+    };
+
+    fetchTokenBalances();
+  }, [address]);
+
+  return walletTokens !== null ? (
     <Section header="Balance">
-      {JSON.stringify(walletBalance)}
-      <Cell
-        after={<Info type="text">{walletBalance}</Info>}
-        before={<Avatar size={48} />}
-      >
-        TON
-      </Cell>
+      {JSON.stringify(walletTokens)}
+      {walletTokens.map((el) => (
+        <Cell
+          key={el.name}
+          after={<Info type="text">{el.balance}</Info>}
+          before={<Avatar size={48} />}
+        >
+          {el.name}
+        </Cell>
+      ))}
     </Section>
   ) : (
     <></>
