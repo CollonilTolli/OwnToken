a0acea86acf7e6c849851852bb0c8a934a4c71da
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-09 04:21:38 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -5,50 +5,33 @@ import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
 import { useTonWallet } from "@tonconnect/ui-react";
 
 export default function BalanceWallet() {
-  const [walletBalance, setWalletBalance] = useState<number | null>(null);
-  const [tokens, setTokens] = useState<any[]>([]);
+  const [wallets, setWallets] = useState<any[] | null>(null);
 
   const wallet = useTonWallet();
   const address = wallet?.account?.address;
-
   useEffect(() => {
-    const url = `https://toncenter.com/api/v2/getAddressInformation?address=${address}`;
+    const url = `https://toncenter.com/api/v3/walletStates?address=${address}`;
     if (address) {
       fetch(url)
         .then(async (response: any) => {
           const res = await response.json();
           console.log(res);
-          setWalletBalance(parseFloat(res.result.balance) / 1e9);
-
-          // Запрос информации о токенах
-          const tokensUrl = `https://toncenter.com/api/v2/getAddressTokens?address=${address}`;
-          fetch(tokensUrl)
-            .then(async (tokensResponse: any) => {
-              const tokensRes = await tokensResponse.json();
-              console.log(tokensRes);
-              setTokens(tokensRes.result);
-            })
-            .catch((error) => console.error(error));
+          setWallets(res.wallets);
         })
         .catch((error) => console.error(error));
     }
-  }, [address, walletBalance]);
+  }, [address, wallets]);
 
-  return walletBalance !== null ? (
+  return wallets !== null ? (
     <Section header="Balance">
-      <Cell
-        after={<Info type="text">{walletBalance}</Info>}
-        before={<Avatar size={48} />}
-      >
-        TON
-      </Cell>
-      {tokens.map((token, index) => (
+      {JSON.stringify(wallets)}
+      {wallets.map((token) => (
         <Cell
-          key={index}
+          key={token.address}
           after={<Info type="text">{token.balance}</Info>}
           before={<Avatar size={48} />}
         >
-          {token.name}
+          TON
         </Cell>
       ))}
     </Section>
