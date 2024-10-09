1e7aacfdf7b7ec00845f728341f85234c0b7335a
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-10 02:43:33 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
 
 import { useTonWallet } from "@tonconnect/ui-react";
+import TonWeb from "tonweb";
 import { fetchJettonData, fetchJettonMetadata } from "@/helpers";
 
 export default function BalanceWallet() {
+  const tonweb = new TonWeb();
   const wallet = useTonWallet();
 
   const [jettonArray, setJettonArray] = useState<any[] | null>(null);
@@ -13,7 +15,7 @@ export default function BalanceWallet() {
   const walletAddress = wallet?.account?.address;
 
   useEffect(() => {
-    const url = `https://tonapi.io/v2/jettons/${walletAddress}`;
+    const url = `https://toncenter.com/api/v3/jetton/wallets?jetton=${walletAddress}`;
     if (wallet) {
       (async () => {
         setJettonArray(await fetchJettonData(url));
@@ -25,7 +27,7 @@ export default function BalanceWallet() {
     if (jettonArray) {
       (async () => {
         const metadataPromises = jettonArray.map(async (jetton) => {
-          const metadataUrl = `https://tonapi.io/v2/jetton/metadata?address=${jetton.jetton}`;
+          const metadataUrl = `https://tonapi.io/v2/jettons/${jetton.jetton}`;
           const metadata = await fetchJettonMetadata(metadataUrl);
           return metadata;
         });
