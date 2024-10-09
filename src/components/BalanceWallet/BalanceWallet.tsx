30a8bb67c4fbdb266d67752d7c73ca3c406a1b82
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-09 04:13:21 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -6,17 +6,29 @@ import { useTonWallet } from "@tonconnect/ui-react";
 
 export default function BalanceWallet() {
   const [walletBalance, setWalletBalance] = useState<number | null>(null);
+  const [tokens, setTokens] = useState<any[]>([]);
 
   const wallet = useTonWallet();
   const address = wallet?.account?.address;
+
   useEffect(() => {
-    const url = `https://toncenter.com/api/v2/jsonRPC`;
+    const url = `https://toncenter.com/api/v2/getAddressInformation?address=${address}`;
     if (address) {
       fetch(url)
         .then(async (response: any) => {
           const res = await response.json();
           console.log(res);
           setWalletBalance(parseFloat(res.result.balance) / 1e9);
+
+          // Запрос информации о токенах
+          const tokensUrl = `https://toncenter.com/api/v2/getAddressTokens?address=${address}`;
+          fetch(tokensUrl)
+            .then(async (tokensResponse: any) => {
+              const tokensRes = await tokensResponse.json();
+              console.log(tokensRes);
+              setTokens(tokensRes.result);
+            })
+            .catch((error) => console.error(error));
         })
         .catch((error) => console.error(error));
     }
@@ -24,13 +36,21 @@ export default function BalanceWallet() {
 
   return walletBalance !== null ? (
     <Section header="Balance">
-      {JSON.stringify(walletBalance)}
       <Cell
         after={<Info type="text">{walletBalance}</Info>}
         before={<Avatar size={48} />}
       >
         TON
       </Cell>
+      {tokens.map((token, index) => (
+        <Cell
+          key={index}
+          after={<Info type="text">{token.balance}</Info>}
+          before={<Avatar size={48} />}
+        >
+          {token.name}
+        </Cell>
+      ))}
     </Section>
   ) : (
     <></>
