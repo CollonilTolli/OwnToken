8588effd8fc78864b0a9cc432d2bb3a1cf1ff594
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-10 02:12:28 +0300

    check get balance

--- src/components/BalanceWallet/BalanceWallet.tsx
+++ src/components/BalanceWallet/BalanceWallet.tsx
@@ -4,13 +4,14 @@ import { Section, Cell, Info, Avatar } from "@telegram-apps/telegram-ui";
 
 import { useTonWallet } from "@tonconnect/ui-react";
 import TonWeb from "tonweb";
-import { fetchJettonData } from "@/helpers";
+import { fetchJettonData, fetchJettonMetadata } from "@/helpers";
 
 export default function BalanceWallet() {
   const tonweb = new TonWeb();
   const wallet = useTonWallet();
 
   const [jettonArray, setJettonArray] = useState<any[] | null>(null);
+  const [jettonMetadata, setJettonMetadata] = useState<any[] | null>(null);
   const walletAddress = wallet?.account?.address;
 
   useEffect(() => {
@@ -23,15 +24,33 @@ export default function BalanceWallet() {
   }, [walletAddress, wallet]);
 
   useEffect(() => {
-    console.log("aaaa");
+    if (jettonArray) {
+      (async () => {
+        const metadataPromises = jettonArray.map(async (jetton) => {
+          const metadataUrl = `https://tonapi.io/v2/jetton/metadata?address=${jetton.jetton}`;
+          const metadata = await fetchJettonMetadata(metadataUrl);
+          return metadata;
+        });
+
+        const metadataArray = await Promise.all(metadataPromises);
+        setJettonMetadata(metadataArray);
+      })();
+    }
   }, [jettonArray]);
 
   return (
     <Section header="Balance">
       {JSON.stringify(wallet)}
-      <Cell after={<Info type="text">aaa</Info>} before={<Avatar size={48} />}>
-        TON
-      </Cell>
+      {jettonMetadata &&
+        jettonMetadata.map((metadata, index) => (
+          <Cell
+            key={index}
+            after={<Info type="text">{metadata.balance}</Info>}
+            before={<Avatar size={48} src={metadata.image} />}
+          >
+            {metadata.symbol || `Jetton ${index + 1}`}
+          </Cell>
+        ))}
     </Section>
   );
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -12,9 +12,31 @@ export async function fetchJettonData(url: string) {
     }
 
     const data = await response.json();
-    return data
+    return data.jetton_wallets
   } catch (error) {
     console.error("Ошибка:", error);
     return null
+  }
+}
+
+
+export async function fetchJettonMetadata(url: string) {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Ошибка:", error);
+    return null;
   }
 }
