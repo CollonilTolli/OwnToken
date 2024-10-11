57233ceac5a0986bc9ae3e3b958b7b60bd9f30ac
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-11 04:53:26 +0300

    check get balance

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,13 @@
 export async function fetchJettonTransfers(JettonId: string, address: string) {
-  const url = `https://tonapi.io/v2/jettons/${JettonId}/transfer/${address}/payload`;
+  const url = `https://tonapi.io/v2/jettons/${address}/holders`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
   const data = await response.json();
-  return data.jetton_transfers;
+  if (data.addresses === null) {
+    return [];
+  } else {
+    return data.addresses;
+  }
 }
