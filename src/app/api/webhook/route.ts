67107063c9fd960a89800cd72bfab504cc02c0fa
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-10-11 07:36:07 +0300

    add start message

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: NextRequest) {
         [
           {
             text: 'connect wallet',
-            url: 'https://own-token.vercel.app/',
+            url: 'https://t.me/own_test_stand_bot?start=0',
           },
         ],
       ],
